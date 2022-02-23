8cb2d966e0234e75a73c21323729681f58ef7cfc
swonderine <44558918+swonderine@users.noreply.github.com>
2022-02-23 15:00:15 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,17 +1,12 @@
 import json
 import requests
 
-# Teekanne
-data = {
-    "sku": "4009300010241"
-}
-
-# New Stuff
+# Dictionary with sku
 data = {
     "sku": "KCGN83228538367005"
 }
 
-
+# Address
 ip_address = 'http://0.0.0.0:5001/similar_others_liked'
 
 r = requests.post(ip_address, json=data)
